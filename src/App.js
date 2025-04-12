e62df08564d463e880546ecc54597971c23e5221
codeswiam <wiamcodes@gmail.com>
2025-04-12 16:12:15 +0200

    Finish Day view

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import CalendarUI from "./components/CalendarUI";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CalendarUI />
+    </div>
+  );
+}
